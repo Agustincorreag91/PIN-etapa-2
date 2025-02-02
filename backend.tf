e9a5837449d0fc0458685a36419f0e8cc772c996
Santiago Bertola <santiagombertola@gmail.com>
2025-02-02 19:07:55 -0300

    sintax adjunstment

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3"{
-    bucket                 = "pin2-practico"
-    region                 = "us-east-1"
+  backend "s3" {
+    bucket = "pin2-practico"
+    region = "us-east-1"
   }
 }
 
