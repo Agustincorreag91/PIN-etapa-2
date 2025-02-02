f19c265e5ef1f63eacbb373f788f41b0be2fee97
Santiago Bertola <santiagombertola@gmail.com>
2025-02-02 19:22:31 -0300

    fix for fmt

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "pin2-practico"
-    key    = "terraform/terraform.tfstate"  # Agregado el parámetro "key"
+    key    = "terraform/terraform.tfstate" # Agregado el parámetro "key"
     region = "us-east-1"
   }
 }
