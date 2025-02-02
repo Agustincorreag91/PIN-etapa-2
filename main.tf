52d062db9b2fe0fffb6f66423d90378c740a10ce
Santiago Bertola <santiagombertola@gmail.com>
2025-02-02 19:27:16 -0300

    fix validate

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "webserver" {
   ami                         = "ami-00c39f71452c08778"
   instance_type               = "t2.micro"
-  key_name                    = pin2-practico
+  key_name                    = "pin2-practico"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
